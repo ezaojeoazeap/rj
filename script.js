e33e214c8c70053117784b4deca6d13b0db7858e
ezaojeoazeap <ezaojeoazeap@users.noreply.github.com>
2026-02-14 18:41:21 +0100

    s

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const card = document.querySelector('.card');
 const buttons = document.querySelector('.buttons');
 const title = document.querySelector('.card h2');
 
-const imgInit = "";
-const imgNo1 = "";
-const imgNo2 = "";
-const imgYes = "";
+const imgInit = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExdms1aWZlbHhmcHB2YzNmcXBnMXk4dnRlMzI1M244c2wwMXVhNm53YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LHlbHhUamxFxo5F878/giphy.gif";
+const imgNo1 = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHBvajQxdGkycHg5OGF0M3V1dWtiYWRkMXk0YXo0bWIxOXYxOWcwbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/z3FfRtL18oZ5CwR4A0/giphy.gif";
+const imgNo2 = "https://imgs.search.brave.com/qZ6q-iTV_mwwt7ZnRkZQtahgy0lgGLeJOVpLQCVecaY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS50ZW5vci5jb20v/OEF1V2dEbWdQQklB/QUFBTS9iYW9uZ29j/LWR0Ym5jdXRpLmdp/Zg.gif";
+const imgYes = "https://imgs.search.brave.com/OVPZbV2fo8cZ8W7MxcNDvUWNSq3mxcjNTN5vTxi3YwI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2U4L2Jh/L2RhL2U4YmFkYTMz/ODUxNTQ4N2VhY2Jl/MDY1M2EyNDRiNDI2/LmpwZw";
 
 let noCount = 0;
 let yesGrow = 1;
